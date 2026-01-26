74f36fd1507aba4f7f8d3a527ac07008491e7bae
Danush <danush.adhitya14@gmail.com>
2026-01-25 17:29:05 -0800

    Error handlers fixed

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ S3_BUCKET= os.getenv("S3_BUCKET_NAME")
 
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///files.db"
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 db= SQLAlchemy(app)
 endpoint_url="http://localhost:4566"
@@ -27,46 +28,79 @@ class File(db.Model):
 with app.app_context():
     db.create_all()
 
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    return {"Error": "File is too large. Maximum allowed size is 16MB"}, 413
+
 @app.route('/',methods=['GET'])
 def index():
     return "Hello World"
 
 @app.route('/list',methods=['GET'])
 def list():
-    files=File.query.order_by(File.date_created).all()
-    for f in files:
-        print(f"ID:{f.id}, Name:{f.filename},Status:{f.status},Date:{f.date_created}")
-    return "Found"
+    if request.method == 'GET':
+        try:
+            files=File.query.order_by(File.date_created).all()
+            result=[
+                {
+                    "id":f.id,
+                    "flename":f.filename,
+                    "status":f.status,
+                    "date_created":f.date_created
+                }
+                for f in files
+            ]
+            return jsonify(result),200
+        except Exception as e:
+            return {"Error":str(e)},500
+
 
 @app.route('/download/<int:id>',methods=['GET'])
 def download(id):
-    file=File.query.get_or_404(id)
-    url=client.generate_presigned_url(
-        "get_object",
-        Params={"Bucket":S3_BUCKET,"Key":file.filename},
-        ExpiresIn=300
+    if request.method == 'GET':
+        try:
+            file=File.query.get_or_404(id)
+            url=client.generate_presigned_url(
+                "get_object",
+                Params={"Bucket":S3_BUCKET,"Key":file.filename},
+                ExpiresIn=300
 
-    )
-    print(url)
-    return "Download"
+            )
+            return {"Download URL":url},200
+        
+        except Exception as e:
+            return {"Error":str(e)},500
 
+
+@app.route('/delete/<int:id>',methods=['DELETE','POST'])
+def delete(id):
+    if request.method == 'DELETE':
+        try:
+            file=File.query.get_or_404(id)
+            deleteobj=client.delete_object(Bucket=S3_BUCKET,Key=file.filename)
+            file.status="DELETED"
+            db.session.commit()
+            return {"Message":f"{file.filename} deleted from S3"},200
+        except Exception as e:
+            return {"Error":str(e)},500
 
 @app.route('/upload',methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         file=request.files['file']
         if file.filename == '':
-            flash('No selected file')
+            return {"Error":"Bad Request-No file provided / invalid payload"},400
         if file.filename:
             filename=secure_filename(file.filename)
-            print(type(filename))
-            client.upload_fileobj(file,Bucket=S3_BUCKET,Key=filename)
-            file=File(filename=filename,status="UPLOADED")
-            db.session.add(file)
-            db.session.commit()
-            return jsonify({
-                "message":"File uploaded successfully"
-            }),200
+            try:
+                client.upload_fileobj(file,Bucket=S3_BUCKET,Key=filename)
+                file=File(filename=filename,status="UPLOADED")
+                db.session.add(file)
+                db.session.commit()
+                return {"Message":f"{filename} uploaded successfully to S3"},201
+            except Exception as e:
+                return {"Error":str(e)},500
+
 
 
 if __name__=="__main__":
